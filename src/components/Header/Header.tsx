86281273a96c1062674c9e084a337ce1a69e2a9d
noumaneAgouzil <noumane.agouzil@atoz-digital.lu>
2022-08-28 20:18:05 +0100

    Added link to project documentation

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,13 +1,16 @@
-import './Header.scss';
+import "./Header.scss";
 
 export default function Header() {
   return (
-    <div className='header-container'>
-      <div className='header'>
-        <img src={require('assets/images/imedia_icon.jpg')} width={'10%'} />
-        <span>Docs</span>
+    <div className="header-container">
+      <div className="header">
+        <img src={require("assets/images/imedia_icon.jpg")} width={"10%"} />
+        <span>
+          {" "}
+          <a href="https://github.com/noumane06/imedia24_front#readme" target="_blank">Docs</a>{" "}
+        </span>
       </div>
-      <h2 className='header-title'>Pokimon list</h2>
+      <h2 className="header-title">Pokimon list</h2>
     </div>
-  )
+  );
 }
